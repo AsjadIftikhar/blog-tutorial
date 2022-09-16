3d9669b1cab482350d087646d2e7b5dcb62d7768
Asjad <iftikharasjad@gmail.com>
2022-09-16 12:32:59 +0500

    Delete Comment Added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,19 @@
 class CommentsController < ApplicationController
+  def show
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+  end
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article), status: :see_other
   end
 
   private
